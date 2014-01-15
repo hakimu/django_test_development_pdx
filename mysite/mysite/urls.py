bb3435d91ad51eb7b2a6c1f195fc00a4d376a19b
Hakimu Jackson <hakimu@newrelic.com>
2014-01-14 23:13:57 -0800

    Initial commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from mysite.views import current_datetime
+
+urlpatterns = patterns('',
+		(r'^time/$', current_datetime),
+		(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
